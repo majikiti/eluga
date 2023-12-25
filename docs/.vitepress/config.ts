69c73e2e625ef8f79336f7f9705bb8f927923f24
YamaD <me@dyama.net>
2023-12-25 17:42:02 +0900

    fix pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress"
 export default defineConfig({
   title: "Eluga Docs",
   description: "The Eluga Docs",
+  base: "/eluga/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
