8a59680318023dd1a40859aa3bb89e4b7ddc687f
jnc-explosion <himajin8634@gmail.com>
2024-01-16 23:54:17 +0900

    fixed

--- src/game/entities/Enemy1.d
+++ src/game/entities/Enemy1.d
@@ -28,15 +28,9 @@ class Enemy1: Enemy {
     auto rb = component!RigidBody;
     if(go.getTag("Missile")) {
       register(new Damage);
-<<<<<<< Updated upstream
-      auto aas = new AudioAsset("damage1.mp3");
-      auto asrc = new AudioSource(aas);
-      asrc.play;
-=======
       //auto aas = new AudioAsset("damage1.mp3");
       //auto asrc = new AudioSource(aas);
       //asrc.play;
->>>>>>> Stashed changes
     }
     if(go.getTag("Ground") && rb.v.y > -1) rb.v.x = 0;
   }
